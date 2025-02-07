f311975a1a03168610a92d87c9983a0935e00566
TaiDT <taidt@splus-software.com.vn>
2025-02-07 22:38:34 +0700

    remove ecr proxy

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -76,7 +76,7 @@ data "aws_iam_policy_document" "ecr" {
     ]
     resources = [
       aws_ecr_repository.app.arn,
-      aws_ecr_repository.proxy.arn,
+      # aws_ecr_repository.proxy.arn,
     ]
   }
 }
