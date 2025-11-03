2327262323bcc1e7d42157e296d1d2e18e567db7
TaiDo <tai272620967@gmail.com>
2025-11-03 23:06:38 +0700

    Update .gitignore, application.properties, and CorsConfig.java for local development

--- src/main/java/vn/hoidanit/jobhunter/config/CorsConfig.java
+++ src/main/java/vn/hoidanit/jobhunter/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://13.229.207.65:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
         configuration.setAllowCredentials(true);
